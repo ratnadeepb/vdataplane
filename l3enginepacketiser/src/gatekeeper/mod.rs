8d59cba14379ed89a1659e7d42c4b3e920877e8b
Ratnadeep Bhattacharya <bhattacharya.ratnadeep@gmail.com>
2021-01-02 15:08:57 +0000

    adding gatekeeper

---
+++ l3enginepacketiser/src/gatekeeper/mod.rs
@@ -0,0 +1,9 @@
+/*
+ * Created on Sat Jan 02 2021:14:41:18
+ * Created by Ratnadeep Bhattacharya
+ */
+
+/// This module is for passing control messages between the packetiser and the clients
+///
+/// It will rely on ØMQ for these communications.
+/// Eventually it would be responsible for authenticating and verifying the clients as well.
