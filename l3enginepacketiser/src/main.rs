f5b72dece7b66a9a82a8ad6ea7865e5a5c6d25f1
Ratnadeep Bhattacharya <bhattacharya.ratnadeep@gmail.com>
2021-01-03 18:51:31 +0000

    communication between main and packetiser through ØMQ

--- l3enginepacketiser/src/main.rs
+++ l3enginepacketiser/src/main.rs
@@ -18,11 +18,16 @@
 
 mod packetiser;
 
+use std::{process::exit, thread::sleep, time::Duration};
+
 use packetiser::{Packetiser, RoutingTable};
 use state::Storage;
+use zmq::Context;
 
 pub const BURST_MAX: usize = 512;
 pub(crate) static TABLE: Storage<RoutingTable> = Storage::new();
+
+const PACKETISER_ZMQ_PORT: &str = "tcp://localhost:5555";
 
 // DEVFLAGS: development flags - remove in production
 #[allow(while_true)]
@@ -30,12 +35,22 @@ pub(crate) static TABLE: Storage<RoutingTable> = Storage::new();
 fn main() {
     packetiser::start();
     let _proc = packetiser::Packetiser::new(BURST_MAX);
+    
     #[cfg(feature = "debug")]
     println!("packetiser created");
     TABLE.set(RoutingTable::new());
+
+    #[cfg(feature = "debug")]
+    println!("packetiser: sending ready msg to main");
+    let context = Context::new();
+    let requester = context.socket(zmq::REQ).unwrap(); // fatal error
+    assert!(requester.connect(PACKETISER_ZMQ_PORT).is_ok());
+    requester.send("Hello", 0).unwrap();
+    #[cfg(feature = "debug")]
+    println!("packetiser: sent ready msg to main");
+
     #[cfg(feature = "debug")]
     println!("packetiser: created routing table");
     while true {
-
     }
 }
