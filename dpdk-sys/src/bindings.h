85dd048a097bfbf950fdcb1fa0bcff06061ea91c
ratnadeepb <ratnadeep.bhattacharya1983@gmail.com>
2021-01-11 15:02:30 -0600

    get the raw packet

--- dpdk-sys/src/bindings.h
+++ dpdk-sys/src/bindings.h
@@ -146,4 +146,7 @@ int _pkt_detect_arp(struct rte_mbuf *buf, uint32_t local_ip);
 /* Create an ARP Response packet */
 struct rte_mbuf *_pkt_arp_response(struct rte_mbuf *pkt, struct rte_mempool *mp);
 
+/* Get the raw packet from rte_mbuf */
+uint8_t *_pkt_raw_pkt(struct rte_mbuf *pkt);
+
 void _pkt_stop_and_close_ports();
